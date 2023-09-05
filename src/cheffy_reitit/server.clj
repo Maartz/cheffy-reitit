5011768e7ab300ad3882abd50413cb8fc95222c5
William Martin <maartz@icloud.com>
2023-09-05 22:47:17 +0200

    put config into edn and setup integrant environment

--- src/cheffy_reitit/server.clj
+++ src/cheffy_reitit/server.clj
@@ -1,6 +1,8 @@
 (ns cheffy-reitit.server
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty])
+            [ring.adapter.jetty :as jetty]
+            [integrant.core :as ig]
+            [environ.core :refer [env]])
   (:gen-class))
 
 (def app
@@ -8,13 +10,31 @@
    (ring/router
     [["/" {:get {:handler (fn [req] {:status 200 :body "Hello World"})}}]])))
 
-(defn start
-  []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "🚀 Server running on port 3000"))
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  (merge config {:port (Integer/parseInt (env :port))}))
+
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]
+  (println (str "🚀 Server running on port: " port))
+  (jetty/run-jetty handler {:port port :join? false}))
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (println "\nStarted app")
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "\nConfigured DB")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [config-file]
+  (let [config (-> config-file slurp ig/read-string)]
+    (-> config ig/prep ig/init)))
 
