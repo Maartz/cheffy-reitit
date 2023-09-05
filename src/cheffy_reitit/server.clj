d272b98b22cbc48420cad348de963ed9a133d533
William Martin <maartz@icloud.com>
2023-09-05 22:17:29 +0200

    basic server

--- src/cheffy_reitit/server.clj
+++ src/cheffy_reitit/server.clj
@@ -1,7 +1,20 @@
 (ns cheffy-reitit.server
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/" {:get {:handler (fn [req] {:status 200 :body "Hello World"})}}]])))
+
+(defn start
+  []
+  (jetty/run-jetty app {:port 3000 :join? false})
+  (println "🚀 Server running on port 3000"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
