100565ceb5ed81f07e48c4b2fdca2b6db35784b5
William Martin <maartz@icloud.com>
2023-09-16 22:14:59 +0200

    clean user profile

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -2,9 +2,7 @@
   (:require [integrant.repl :as ig-repl]
             [integrant.core :as ig]
             [integrant.repl.state :as state]
-            [cheffy-reitit.server]
-            [next.jdbc :as jdbc]
-            [next.jdbc.sql :as sql]))
+            [cheffy-reitit.server]))
 
 (ig-repl/set-prep!
  (fn [] (-> "resources/config.edn" slurp ig/read-string)))
@@ -16,13 +14,3 @@
 
 (def app (-> state/system :cheffy-reitit/app))
 (defn db [] (-> state/system :db/postgres))
-
-(comment
-  (app {:request-method :get
-        :uri "/swagger.json"})
-  (jdbc/execute! db ["SELECT * FROM recipe WHERE public = true"])
-  (sql/find-by-keys db :recipe {:public true})
-  (go)
-  (halt)
-  (reset-all)
-  (reset))
